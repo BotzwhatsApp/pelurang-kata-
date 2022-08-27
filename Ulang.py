52a9d580f1292a4a7f88d18951352c3726eba3e6
©Botz GexusR <99302576+BotzwhatsApp@users.noreply.github.com>
2022-08-27 08:16:12 +0700

    Create Ulang.py

---
+++ Ulang.py
@@ -0,0 +1,30 @@
+#!/bin/python
+# program pelurang kata
+
+# module
+Import os,sys,time
+From time import sleep
+
+# tampilan
+os.system( 'clear')
+os.system('figlet contoh')
+sleep(1)
+print(" (+) By Mr Virus Spm (+)')
+print(" print(" ')
+print('Masukan Kata"mu Stah ")
+kata = input('=> ') print("Masukam Jumlah Perulangan')
+jumlah = int(input('=>'))
+
+# data
+
+try: for i in range(jumlah):
+
+print('=>' + kata) sleep(0.05)
+
+print('=> kata)
+
+sleep(0.05)
+
+except:
+
+print('berhasil')
